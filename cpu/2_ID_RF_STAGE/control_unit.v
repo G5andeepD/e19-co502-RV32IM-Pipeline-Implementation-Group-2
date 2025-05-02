54ea4430808f2ae9ca1dc9a42b77d088afb3d555
G5andeepD <e19063@eng.pdn.ac.lk>
2025-05-02 14:49:31 +0530

    support immediate instructions

--- cpu/2_ID_RF_STAGE/control_unit.v
+++ cpu/2_ID_RF_STAGE/control_unit.v
@@ -22,7 +22,7 @@ module control_unit (
                   5'b00000;
     assign #3 reg_write_enable = 1'b1; // Enable register write for all instructions
     assign #3 op1_sel = 1'b1; // Select first operand from register file
-    assign #3 op2_sel = 1'b0; // Select second operand from register file
+    assign #3 op2_sel = (opcode == `OPCODE_I_TYPE) ? 1'b1 : 1'b0; 
     assign #2 mem_write = 2'b00; // No memory write for all instructions
     assign #2 mem_read = 2'b00; // No memory read for all instructions
     assign #2 branch_jump = 3'b000; // No branch or jump for all instructions
