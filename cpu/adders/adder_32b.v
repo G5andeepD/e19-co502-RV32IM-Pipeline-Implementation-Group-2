13ef74e193fff818217d0c528a19c2d6990cfc8f
G5andeepD <e19063@eng.pdn.ac.lk>
2025-06-07 19:11:20 +0530

    debug

--- cpu/adders/adder_32b.v
+++ cpu/adders/adder_32b.v
@@ -5,5 +5,5 @@ module adder_32b(
     input [31:0] b, // Second operand
     output [31:0] sum // Sum output
 );
-    assign sum = a + (b<<2); // Perform addition with a delay of 1 time unit
+    assign sum = a + b + 4; 
 endmodule
